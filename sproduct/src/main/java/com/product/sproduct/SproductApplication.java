7abbd1f57bcb556176b441814711c8cc835ed975
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-07-20 11:30:40 +0530

    initialize all -> microservice + spring + node + react

---
+++ sproduct/src/main/java/com/product/sproduct/SproductApplication.java
@@ -0,0 +1,13 @@
+package com.product.sproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproductApplication.class, args);
+	}
+
+}
---
+++ spurchase/src/main/java/com/purchase/spurchase/SpurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.purchase.spurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpurchaseApplication.class, args);
+	}
+
+}
